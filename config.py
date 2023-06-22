36f3a50680acba876a80048aee61c961a4ec4711
Анастасия В <127120312+anastasiavas77@users.noreply.github.com>
2023-06-22 11:48:49 +0500

    telegram bot

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN = "6197250568:AAEmf348gZVjU4X96WFmE3e2h2JBFXz8zO0"
+
+keys = {'доллар' : 'USD',
+        'евро' : 'EUR',
+        'рубль' : 'RUB'}
---
+++ extensions.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from config import *
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException('Не удалось перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не смог обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не смог обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не смог обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = float(json.loads(r.content)[keys[base]])
+        return total_base*amount
